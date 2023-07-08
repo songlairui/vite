2105452eae2fa7c0edbaa8764ec42edc7be855c3
songlairui <cnre@live.cn>
2023-07-09 01:52:29 +0800

    feat(playground): add shortcuts

--- playground/cli/vite.config.js
+++ playground/cli/vite.config.js
@@ -6,6 +6,18 @@ export default defineConfig({
     headers: {
       'Cache-Control': 'no-store',
     },
+    shortcuts: [
+      // built-in usage: r u o c q
+      {        
+        key: 'f',
+        description: 'server --info--> client',
+        action(server) {
+          if ('ws' in server) {
+            server.ws.send('custom.msg', 'info from dev-cli')
+          }
+        }
+      }
+    ]
   },
   build: {
     //speed up build
